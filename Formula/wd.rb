fda618c450378248542b17f04d2697d1b25250f8
Guangxiong Lin <hi@gxlin.org>
2022-10-28 18:50:01 +0800

    Upgrade wd to v0.0.3

--- Formula/wd.rb
+++ Formula/wd.rb
@@ -1,12 +1,15 @@
 class Wd < Formula
   desc "A simple note taking tool."
+  version "v0.0.3"
   homepage "https://git.gxlin.org/wd/"
-  url "https://git.gxlin.org/wd/snapshot/wd-0.0.1.tar.gz"
-  sha256 "1f4775924a4a2f94bed4b925d34f21585ec2cb37a354556ecc1d661f88d11792"
   license "GPL-3.0"
+
+  url "https://git.gxlin.org/wd/snapshot/wd-#{version}.tar.gz"
+  sha256 "dc6c1a7f36d6dbf2f8c1f7a9c1fd605f17f633750a5acdf7f997f5f6152c470a"
+
   head "https://git.gxlin.org/wd.git", branch: "master"
 
   def install
-    bin.install "wd", "wd-journal"
+    bin.install "wd", "wd-journal", "wd-edit"
   end
 end
